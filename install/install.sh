dff5b133e89f94cfec31421415f041cf065d4571
roli <roli@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-06-08 17:28:33 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/vegsoup@54 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- install/install.sh
+++ install/install.sh
@@ -1,1 +1,7 @@
-R CMD INSTALL -l /Users/roli/Library/R/2.11/library pkgs /Users/roli/Desktop/vegsoup
+R CMD build /Users/roli/Documents/vegsoup/pkg
+R CMD check vegsoup_0.1-1.tar.gz
+
+R CMD INSTALL -l /Users/roli/Library/R/2.11/library /Users/roli/Documents/vegsoup/pkg
+
+* Run R CMD build to build the package tarball.
+* Run R CMD check to check the package tarball.

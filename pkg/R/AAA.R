e842ddadabffc831119a1689b62a017ec9ba5b4b
roli <roli@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-06-08 16:30:40 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/vegsoup@39 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/AAA.R
+++ pkg/R/AAA.R
@@ -1,46 +1,14 @@
-#	translates database format to basic class structure
-#	species and sites records are stored in long format
-#	validity methods are implemented
+#	import classes
+setOldClass("duleg")
+setOldClass("tabdev")
+setOldClass("disdiam")
+setOldClass("partana")
+setOldClass("silhouette")
 
-setClass("Vegsoup",
-	representation(
-	species.long = "data.frame", # in long format, casting by Vegsoup
-	sites.long = "data.frame", # in long format, casting by Vegsoup
-	taxonomy = "data.frame", # in long format
-	scale = "list",
-	layers = "character",
-	group = "integer",
-#	sp.raster = "raster",
-	sp.points = "SpatialPointsDataFrame",
-	sp.polygons = "SpatialPolygonsDataFrame"),
-	
-	validity = function (object) {
-		if (length(object@taxonomy) > 0) {
-			valid.abbr <-
-				all(object@species.long$abbr %in% object@taxonomy$abbr)
-			valid.sites <-
-				all(object@species.long$plot %in% object@sites.long$plot)
-
-			if (valid.abbr && valid.sites) {
-#				cat("\ntaxomomy lookup table matching",
-#					"\nspecies and sites matching",
-#					"\nslots: species, sites and taxonomy",
-#					"assigned")
-				TRUE
-			} else {
-				if (!valid.abbr) {
-					warning("taxomomy lookup table has",
-						"non matching taxa!")
-					FALSE		
-				}
-				if (!valid.sites) {
-					warning("\nspecies and sites has",
-						"non matching plots!")
-					FALSE
-				}
-			}
-		} else {
-			warning("no taxonomy lookup table supplied!\n")
-		}
-	}
-)
+#	VegsoupDataPartition methods
+setOldClass("agnes")
+setOldClass("pam")
+setOldClass("optpart")
+setOldClass("isopam")
+setOldClass("twins")
+setOldClass("partition")

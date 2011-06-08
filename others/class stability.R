4b8dad16389637a2d9bad8d8225d46731fc49f32
roli <roli@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-06-08 15:28:24 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/vegsoup@6 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ others/class stability.R
@@ -0,0 +1,181 @@
+#   ---------------------------------------------------------
+#   ---         Function Classification Stability         ---
+#   ---------------------------------------------------------
+
+   library(vegan)
+   library(isopam)
+   library (tcltk)
+
+#   ---------------------------------------------------------
+
+   no.cycles<-200
+   no.clusters<-8
+
+   pb <- txtProgressBar (min = 1, max = no.cycles, char = '.',
+        width = 45, style = 3, title = 'Calculation progress')
+
+   setTxtProgressBar (pb, 1)
+
+#  ----------------------------------------------------------
+#  ---                                                    ---
+#  ---              Table Classification                  ---
+#  ---                (Alternatives)                      ---
+#  ---                                                    ---
+#  ----------------------------------------------------------
+
+#  ..........................................................
+#  ---                K-means clustering                  ---
+
+#   prvni<-kmeans(JUICE.table,no.clusters,nstart=25)[1]
+#  ..........................................................
+
+#  ..........................................................
+#  ---                  Wards method                      ---
+
+   sih<-vegdist(JUICE.table,method='euclidean')	
+   prvni<-cutree(hclust(sih,method='ward'),no.clusters)
+#  ..........................................................
+
+#  ..........................................................
+#  ---             Flexible beta clustering               ---
+
+    alpha <- 0.625  #    beta = 1-2*alpha
+#   sih<-agnes(vegdist(JUICE.table,method='bray'), method='flexible', par.meth=c(alpha,alpha,1-2*alpha,0))
+#   prvni<-cutree(as.hclust(sih),no.clusters)
+#  ..........................................................
+
+#  ..........................................................
+#  ---                        Isopam                     ---
+
+#   prvni<-isopam(JUICE.table,c.fix=no.clusters)[3]
+#  ..........................................................
+
+	prv<-as.matrix(data.frame(prvni))
+	plot.no <- dim(JUICE.table)[1]
+	spec.no <- dim(JUICE.table)[2]
+	lambda <- 0
+	rnd_lambda <- 0
+	lb <- vector (mode = 'numeric', length = no.cycles)
+
+	for (cycle in 1:no.cycles)
+	{
+
+
+
+	if (round(cycle/10,0) == cycle/10)
+		{
+		setTxtProgressBar (pb, cycle)
+
+		}
+
+#   ---------------------------------------------------------
+#   ---  Subset selection without replacement             ---
+#   ---------------------------------------------------------
+
+		q<-c(1:plot.no)
+   		r<-sample(q,replace = T)
+   		s<-sort(r)
+   		t<-unique(s)
+   		JUICE.subtable <- JUICE.table[t,]
+
+#   ---------------------------------------------------------
+#   ---         Removal of empty species                  ---
+#   ---------------------------------------------------------
+
+   		JUICE.sstable <- JUICE.subtable[, colSums(JUICE.subtable) > 0]
+   		subplot.no <- dim(JUICE.sstable)[1]
+   		subspec.no <- dim(JUICE.sstable)[2]
+
+#  ----------------------------------------------------------
+#  ---                                                    ---
+#  ---              Subset classification                 ---
+#  ---                (Alternatives)                      ---
+#  ---                                                    ---
+#  ----------------------------------------------------------
+
+#  ..........................................................
+#  ---                K-means clustering                  ---
+
+#   druha<-kmeans(JUICE.sstable,no.clusters,nstart=10)[1]
+#  ..........................................................
+
+#  ..........................................................
+#  ---                  Wards method                      ---
+
+   sih<-vegdist(JUICE.sstable,method='euclidean')	
+   druha<-cutree(hclust(sih,method='ward'),no.clusters)
+#  ..........................................................
+
+#  ..........................................................
+#  ---             Flexible beta clustering               ---
+
+#   sih<-agnes(vegdist(JUICE.sstable,method='bray'), method='flexible', par.meth=c(alpha,alpha,1-2*alpha,0))
+#   druha<-cutree(as.hclust(sih),no.clusters)
+#  ..........................................................
+
+#  ..........................................................
+#  ---                        Isopam                     ---
+
+#   druha<-isopam(JUICE.sstable,c.fix=no.clusters)[3]
+#  ..........................................................
+
+
+   		dru<-as.matrix(data.frame(druha))
+
+#   ---------------------------------------------------------
+#   ---         Crosstabulation                           ---
+#   ---------------------------------------------------------
+
+   		crosstab<-array(0,dim=c(no.clusters,no.clusters))
+   		crossran<-array(0,dim=c(no.clusters,no.clusters))
+   		prv.temp <- prv[rownames (prv) %in% rownames (dru),] #tohle vybere z prv jenom ty radky se snimky obsazenymi take v dru (na to slouzi funkce A %in% B, coz znamena ze A bude TRUE pokud se vyskytuje i v B, jinak bude FALSE; hranaty zavorky nakonec z prv vyseknou jenom ty radky, ktere jsou TRUE)
+   		crosstab <- table (prv.temp, dru) # vytvori kontingencni tabulku
+
+
+#   ---------------------------------------------------------
+#   ---         Raw lambda calculation                        ---
+#   ---------------------------------------------------------
+
+   		crossR<-apply(crosstab,1,function(x) max(x))
+   		crossC<-apply(crosstab,2,function(x) max(x))
+   		F1<-sum(crossR)
+   		F2<-sum(crossC)
+   		F5<-2 * sum(crosstab)
+   		F3<-max(rowSums(crosstab))
+   		F4<-max(colSums(crosstab))
+   		lb[cycle]=(F1+F2-F3-F4)/(F5-F3-F4)
+
+#   ---------------------------------------------------------
+#   ---         Random lambda calculation (10 times more) ---
+#   ---------------------------------------------------------
+
+		for (duna in 1:10)
+			{
+   			a<-factor(sample(1:no.clusters,subplot.no, replace=T),levels=1:no.clusters)
+   			b<-factor(sample(1:no.clusters,subplot.no, replace=T),levels=1:no.clusters)
+   			tab=table(a,b)
+   			crossR<-apply(tab,1,function(x) max(x))
+   			crossC<-apply(tab,2,function(x) max(x))
+   			F1<-sum(crossR)
+   			F2<-sum(crossC)
+   			F5<-2 * sum(tab)
+   			F3<-max(rowSums(tab))
+   			F4<-max(colSums(tab))
+   			rnd_lambda=rnd_lambda+(F1+F2-F3-F4)/(F5-F3-F4)
+			}
+		}
+
+
+#   ---------------------------------------------------------
+#   ---         Final modified lambda calculation         ---
+#   ---------------------------------------------------------
+
+	sort (lb)
+   	lambda=sum(lb)
+   	lambda=lambda/no.cycles
+   	rnd_lambda=rnd_lambda/(no.cycles*10)
+   	mod_lambda=(lambda-rnd_lambda)/(1-rnd_lambda)
+   	mod_lambda
+
+#   ---------------------------------------------------------
+
---
+++ others/tvveg.R
@@ -0,0 +1,104 @@
+function (db, tv_home, tax = TRUE, convcode = TRUE, lc = c("layer", 
+    "mean", "max", "sum", "first"), pseudo = list(lc.1, "LAYER"), 
+    values = "COVER_PERC", concept, names = c("short", "long"), 
+    dec = 0, obs, refl, spc, site, RelScale, sysPath = FALSE, 
+    ...) 
+{
+    lc <- match.arg(lc)
+    names = match.arg(names)
+    if (missing(tv_home)) 
+        tv_home <- tv.home(sysPath = sysPath, ...)
+    if (missing(obs)) 
+        obs <- tv.obs(db, tv_home)
+    if (suppressWarnings(any(obs < -1e+05, na.rm = TRUE))) 
+        cat(paste("\n WARNING! Values less than -100,000. \n WARNING! tvabund.dbf may be corrupt. \n WARNING! Please correct by reexporting e.g. with OpenOffice."), 
+            quote = FALSE)
+    if (!missing(spc)) {
+        cat("\n Selecting species ... \n")
+        obs <- obs[!obs$SPECIES_NR %in% spc, ]
+    }
+    if (missing(refl)) 
+        refl <- tv.refl(db[1], tv_home = tv_home)
+    cat("Taxonomic reference list: ", refl, "\n")
+    if (tax) 
+        obs <- tv.taxval(obs = obs, refl = refl, tv_home = tv_home, 
+            sysPath = sysPath, ...)
+    if (convcode) {
+        cat(paste("\n converting cover code ... \n"))
+        if (missing(RelScale)) {
+            RelScale <- tv.site(db, tv_home = tv_home, quiet = TRUE, 
+                sysPath, iconv = NULL)[, c("RELEVE_NR", "COVERSCALE")]
+            obs <- tv.coverperc(obs = obs, RelScale = RelScale, 
+                tv_home = tv_home, ...)
+        }
+        else {
+            obs <- tv.coverperc(db[1], obs, tv_home = tv_home, 
+                ...)
+        }
+    }
+    else {
+        if (!any(names(obs) == values)) 
+            stop(paste(values, " could not be found."))
+        obs[, values] <- type.convert(as.character(obs[, values]))
+        if (is.factor(obs[, values])) {
+            lc = "first"
+            warning("Multiple occurrences of a species in one releve not supported without cover code conversion. Only first value used!")
+        }
+    }
+    if (!is.null(pseudo)) {
+        cat("\n creating pseudo-species ... \n")
+        if (length(pseudo[[2]]) > 1) 
+            stop("Function to differentiate more than one species-plot attribute not yet implemented. Please contact jansen@uni-greifswald.de.")
+        obs$COMB <- pseudo[[1]][, 2][match(obs[, pseudo[[2]]], 
+            pseudo[[1]][, 1])]
+        collab <- paste(obs$SPECIES_NR, obs$COMB, sep = ".")
+    }
+    else collab <- as.vector(obs$SPECIES_NR)
+    rowlab <- as.vector(obs$RELEVE_NR)
+    cat("\n combining occurrences using type", toupper(lc), "and creating vegetation matrix ... \n")
+    layerfun <- function (x) round((1 - prod(1 - x/100)) * 100, 
+        dec)
+    results <- switch(lc, layer = tapply(obs[, values], list(rowlab, 
+        collab), layerfun), mean = tapply(obs[, values], list(rowlab, 
+        collab), mean), max = tapply(obs[, values], list(rowlab, 
+        collab), max), sum = tapply(obs[, values], list(rowlab, 
+        collab), sum), first = tapply(obs[, values], list(rowlab, 
+        collab), first.word))
+    results[is.na(results)] <- 0
+    results <- as.data.frame(results)
+    if (names %in% c("short", "long") & is.null(pseudo)) {
+        cat(paste("\n", "replacing species numbers with ", names, 
+            "names ...", "\n", sep = ""))
+        species <- tax(as.numeric(colnames(results)), tax = FALSE, 
+            refl = refl, tv_home = tv_home, sysPath = sysPath, 
+            ...)
+        if (names == "short") 
+            colnames(results) <- species$LETTERCODE[match(colnames(results), 
+                species$SPECIES_NR)]
+        if (names == "long") 
+            colnames(results) <- gsub(" ", "_", species$ABBREVIAT[match(colnames(results), 
+                species$SPECIES_NR)])
+    }
+    if (names %in% c("short", "long") & !is.null(pseudo)) {
+        st <- unlist(strsplit(colnames(results), ".", fixed = TRUE))
+        colspc <- st[seq(1, length(st), 2)]
+        species <- tax(as.numeric(colspc), tax = FALSE, refl = refl, 
+            tv_home = tv_home, sysPath = sysPath, ...)
+        ll <- st[seq(2, length(st), 2)]
+        if (names == "short") 
+            coln <- as.character(species$LETTERCODE[match(as.numeric(colspc), 
+                species$SPECIES_NR)])
+        if (names == "long") 
+            coln <- gsub(" ", "_", species$ABBREVIAT[match(as.numeric(colspc), 
+                species$SPECIES_NR)])
+        cn <- replace(coln, which(ll != 0), paste(coln, ll, sep = ".")[ll != 
+            0])
+        colnames(results) <- cn
+        cat("\n replacing species numbers with ", names, " names ... \n")
+    }
+    if (any(is.na(colnames(results)))) 
+        warning("Some taxa without names, check reference list!")
+    class(results) <- c("veg", "data.frame")
+    results <- results[, order(names(results))]
+    results
+}

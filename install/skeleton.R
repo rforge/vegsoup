3f24355fa36d1133807ce828df6db6779a83707d
roli <roli@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-06-08 16:53:36 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/vegsoup@43 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- install/skeleton.R
+++ install/skeleton.R
@@ -3,5 +3,5 @@ setwd("~/Documents/vegsoup/pkg/R")
 objs <- system("ls", intern = TRUE)
 
 package.skeleton("vegsoup", code_files = objs,
-	path = "~/Desktop",
+	path = "/Users/roli/Desktop/vegsoup",
 	namespace = TRUE, force = TRUE)

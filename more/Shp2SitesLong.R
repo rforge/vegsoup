71e54f4c75e8e9f4c21257e0c5e45634b75332ad
roli <roli@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-06-09 15:22:43 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/vegsoup@135 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- more/Shp2SitesLong.R
+++ more/Shp2SitesLong.R
@@ -5,25 +5,24 @@ Shp2SitesLong <- function (dsn, layer, plot.column, elevation.column, round = TR
 if (missing(plot.column)) {
 	stop ("please supply column name indicating plot ids in dbf file")
 }
- {
-		
-} 
+
 require(rgdal)
 pt <- readOGR(dsn, layer)
-names(pt) <- to.lower(names(pt))
+names(pt) <- tolower(names(pt))
 pt <- spTransform(pt, CRS("+init=epsg:4326"))
 
 if (missing(elevation.column)) {
 	df <- data.frame(coordinates(pt),
-		as.character(pt@data[,names(pt) == tolower(plot.column)]))
+		as.character(pt@data[,names(pt) == tolower(plot.column)]),
+		stringsAsFactors = FALSE)
 } else {
 	df <- data.frame(coordinates(pt),
+		as.numeric(as.character(pt@data[,names(pt) == tolower(elevation.column)])),
 		as.character(pt@data[,names(pt) == tolower(plot.column)]),
-		as.character(pt@data[,names(pt) == tolower(elevation.column)]))
-	
+		stringsAsFactors = FALSE)
 }
 
-if (dim(coordinates(pt))[2] == 2) {
+if (dim(coordinates(pt))[2] == 2 & missing(elevation.column)) {
 	
 	names(df)[1:3] <- c("longitude", "latitude", "plot")
 	res <- data.frame(as.character(df$plot), stack(df),
@@ -36,7 +35,26 @@ if (dim(coordinates(pt))[2] == 2) {
 		round(res$value[res$variable == "longitude"], 6)
 
 }
-if (dim(coordinates(pt))[2] == 3) {#
+
+if (dim(coordinates(pt))[2] == 2 & !missing(elevation.column)) {
+	names(df)[1:4] <- c("longitude", "latitude", "altitude", "plot")
+
+	res <- data.frame(as.character(df$plot),
+		stack(df, select = 1:3),
+		stringsAsFactors = FALSE)
+	res	 <- res[, c(1,3,2)]
+	res[,3] <- as.numeric(as.character(res[,3]))
+	names(res) <- c("plot", "variable", "value")
+	res$value[res$variable == "longitude"] <-
+		round(res$value[res$variable == "longitude"], 6)
+	res$value[res$variable == "latitude"] <-
+		round(res$value[res$variable == "longitude"], 6)
+	res$value[res$variable == "altitude"] <-
+		round(res$value[res$variable == "altitude"], 0)
+
+}
+
+if (dim(coordinates(pt))[2] == 3) {
 	names(df)[1:4] <- c("longitude", "latitude", "altitude", "plot")
 	res <- data.frame(as.character(df$plot), stack(df),
 		stringsAsFactors = FALSE)
@@ -47,7 +65,7 @@ if (dim(coordinates(pt))[2] == 3) {#
 	res$value[res$variable == "latitude"] <-
 		round(res$value[res$variable == "longitude"], 6)
 	res$value[res$variable == "altitude"] <-
-		round(res$value[res$variable == "altitude"], 0)	
+		round(res$value[res$variable == "altitude"], 0)
 
 }
 
@@ -58,6 +76,6 @@ return(invisible(res))
 df <- Shp2SitesLong(
 	dsn = "/Users/roli/Documents/javakheti/dta/shp/pt_plots",
 	layer = "pt_plots",
-	plot.column = "PLOT",
-	elevation.column = "ELE")
+	plot.column = "plot",
+	elevation.column = "ele")
 write.csv2(df, "~/foo.csv", row.names = FALSE)

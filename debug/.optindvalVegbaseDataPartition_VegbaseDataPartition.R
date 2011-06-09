cf90d06211d77996618d69b16a5df2a4aa25ec9f
roli <roli@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-06-09 10:29:42 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/vegsoup@102 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- debug/.optindvalVegbaseDataPartition_VegbaseDataPartition.R
+++
@@ -1,7 +0,0 @@
-setMethod(".optindvalVegbaseDataPartition",
-    signature(obj = "VegbaseDataPartition"),
-    function (obj) 
-    {
-        stop("Need a definition for the method here")
-    }
-)

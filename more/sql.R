a8d92991b4a16ddde134db0f9449fa9af7b278f4
roli <roli@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-01-15 19:03:31 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/vegsoup@331 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- more/sql.R
+++
@@ -1,9 +0,0 @@
-SELECT plot, abbr, layer, cov FROM table WHERE PLOT LIKE fü%
-
-SELECT plot, abbr, layer, cov FROM table WHERE PLOT LIKE
-
-prjs <- c("a", "b", "c")
-
-test <- unique(table$plot)
-paste(" OR PLOT LIKE", prjs, collapse = "")
-prj1 OR LIKE prj2 OR LIKE prj_i

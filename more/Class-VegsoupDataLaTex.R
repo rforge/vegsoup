ce9fbcbd7e6d126bc4cc9e799bfafeb648b182be
roli <roli@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-01-15 19:03:03 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/vegsoup@329 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- more/Class-VegsoupDataLaTex.R
+++
@@ -1,9 +0,0 @@
-setClass("VegsoupDataLaTex",
-	representation(
-	species = "VegsoupDataLaTexSpecies",
-	sites = "VegsoupDataLaTexSites",
-	file.name = "character",
-	longtable = "logical",
-	part = "numeric",
-	LaTex = "list") # to be assigend by '.VegsoupLaTexParse'
-	)
--- more/Class-VegsoupDataLatexSites.R
+++
@@ -1,5 +0,0 @@
-setClass("VegsoupDataLaTexSites",
-	representation(
-	sites.latex = "matrix", # sites & sites.extended should be a list
-	sites.latex.extended = "matrix",
-	caption = "character"))

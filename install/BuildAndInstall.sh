66cdd21cff7aa8bab99ffd3b0522cc3e2f57d4ac
roli <roli@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-05-29 14:57:11 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/vegsoup@362 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- install/BuildAndInstall.sh
+++ install/BuildAndInstall.sh
@@ -1,5 +1,6 @@
 R CMD build /Users/roli/Documents/vegsoup/pkg
-R CMD INSTALL -d -l /Users/roli/Library/R/2.14/library vegsoup_0.1-3.tar.gz
+R CMD INSTALL -d -l /Users/roli/Library/R/2.15/library vegsoup_0.1-3.tar.gz
+
 
 # --fake		do minimal install for testing purposes
 

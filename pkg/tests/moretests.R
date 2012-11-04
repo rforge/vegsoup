e29b4d5c03dce0f5a21f3041e6c2985efa68abf7
roli <roli@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-11-04 18:52:35 +0000

    library(vegsoup)
    data(testdata)
    dta <- VegsoupData(Vegsoup(species, sites, taxonomy,
    	scale = list(scale = "Braun-Blanquet")))
    summary(dta)
    Abbreviation(dta)
    AbundanceScale(dta)
    
    dta <- VegsoupData(Vegsoup(species, sites, taxonomy, group = c(rep(1, 3), rep(2, 3)),
    	scale = list(scale = "Braun-Blanquet")))
    AprioriGrouping(dta)
    AbundanceScale(BraunBlanquetReduce(dta))
    
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/vegsoup@422 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tests/moretests.R
+++ pkg/tests/moretests.R
@@ -1,5 +1,13 @@
-data(testspecies)
-data(testsites)
-data(testtaxonomy)
+library(vegsoup)
 
-Vegsoup(species, sites, taxonomy)
+data(testdata)
+dta <- VegsoupData(Vegsoup(species, sites, taxonomy,
+	scale = list(scale = "Braun-Blanquet")))
+summary(dta)
+Abbreviation(dta)
+AbundanceScale(dta)
+
+dta <- VegsoupData(Vegsoup(species, sites, taxonomy, group = c(rep(1, 3), rep(2, 3)),
+	scale = list(scale = "Braun-Blanquet")))
+AprioriGrouping(dta)
+AbundanceScale(BraunBlanquetReduce(dta))

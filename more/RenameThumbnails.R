724d4d4683c89a91fbf7285ff35d0566d93ea0d4
roli <roli@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-11-16 16:18:56 +0000

    kw <- read.csv2("/Volumes/roli/Desktop/jpg/keywords.csv", stringsAsFactors = FALSE)
    kw[,1] <- as.character(kw[,1])
    kw[,2] <- as.character(kw[,2])
    kw[,2] <- sapply(strsplit(kw[,2], ",", fixed = TRUE), function (x) x[3])
    
    kw <- apply(kw, 1, function (x) paste("mv /Volumes/roli/Desktop/jpg/",
    	gsub(" ", "\\ ", x[1], fixed = TRUE), ".jpg ", # if there are blanks in filenames
    	"/Volumes/roli/Desktop/jpg/", x[2], ".jpg", sep = ""))
    	
    sapply(kw, system)	
    
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/vegsoup@436 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- more/RenameThumbnails.R
+++ more/RenameThumbnails.R
@@ -1,14 +1,11 @@
-library(gdata)
-
-kw <- read.csv2("/Users/roli/Documents/jpg/keywords.csv", stringsAsFactors = FALSE)
-kw <- read.xls("/Users/roli/Documents/jpg/keywords.xls")
+kw <- read.csv2("/Volumes/roli/Desktop/jpg/keywords.csv", stringsAsFactors = FALSE)
 
 kw[,1] <- as.character(kw[,1])
 kw[,2] <- as.character(kw[,2])
-kw[,2] <- sapply(strsplit(kw[,2], ",", fixed = TRUE), function (x) x[2])
+kw[,2] <- sapply(strsplit(kw[,2], ",", fixed = TRUE), function (x) x[3])
 
-kw <- apply(kw, 1, function (x) paste("mv /Users/roli/Documents/jpg/",
+kw <- apply(kw, 1, function (x) paste("mv /Volumes/roli/Desktop/jpg/",
 	gsub(" ", "\\ ", x[1], fixed = TRUE), ".jpg ", # if there are blanks in filenames
-	"/Users/roli/Documents/jpg/", x[2], ".jpg", sep = ""))
+	"/Volumes/roli/Desktop/jpg/", x[2], ".jpg", sep = ""))
 	
 sapply(kw, system)	

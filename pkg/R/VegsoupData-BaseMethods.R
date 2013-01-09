80dea4700d92ffd17f244a9b9a04c8012dc79ce5
roli <roli@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-01-09 09:29:44 +0000

    \name{head} \alias{head} \alias{head-methods} \alias{head,VegsoupData-method} \alias{head,VegsoupDataPartition-method} \alias{tail} \alias{tail-methods} \alias{tail,VegsoupData-method}
    \title{
    Return the First or Last Part of the Species Matrix
    }
    \description{
    Returns the first or last parts of the species matrix.
    }
    \usage{
    head(x, n, choice, mode, ...) 
    tail(x, ...)
    }
    % n = 6L
    %- maybe also 'usage' for other objects documented here.
    \arguments{
      \item{x}{
    An object inheriting from class \code{VegsoupData}.
    }
      \item{n}{
    A single integer. If positive, size for the resulting object. If negative, all but the n last/first number of elements of x.
    }
      \item{choice}{
    Display first row of species matrix (the default) or sites data frame.
    }
      \item{mode}{
    If choice is \dQuote{species} of which mode should the returned matrix be reported. If missing the presence/absence mode of the species matrix is printed. See \code{\link{as.logical}}, \code{\link{as.numeric}}, and \code{\link{as.character}}, respectively.
    }
      \item{\dots}{
    not used
    }
    }
    
    \details{
    %%  ~~ If necessary, more details than the description above ~~
    }
    \value{
    An object like \code{as.character(x)} but generally smaller.
    \author{
    Roland Kaiser
    }
    
    \seealso{
    See \code{\link{head}} and \code{\link{tail}} in package \pkg{utils}.
    }
    \examples{
    dta <- VegsoupData(Vegsoup(species, sites, taxonomy,
    		scale = list(scale = "Braun-Blanquet")))
    head(dta)
    }
    \keyword{methods}
    
    
    
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/vegsoup@454 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/VegsoupData-BaseMethods.R
+++ pkg/R/VegsoupData-BaseMethods.R
@@ -509,7 +509,7 @@ setReplaceMethod("$",
 	function (x, name, value) {
 		if (!("sites" %in% slotNames(x)))
 			stop("no $<- method for object without attributes")		
- 			x@sites[[name]] = value 	
+ 			x@sites[[name]] <- value 	
 		return(x)		
 	}
 )
@@ -522,8 +522,7 @@ setReplaceMethod("$",
 setMethod("[",
     signature(x = "VegsoupData",
     i = "ANY", j = "ANY", drop = "missing"),
-    function (x, i, j, ..., drop = TRUE)
-    {
+    function (x, i, j, ..., drop = TRUE) {
 	    #	debug
 	    #	x = dta; i = 1; j <- c(4,7,9,1,12); j <- rep(TRUE, ncol(x))
 		#	x <- prt; i = Partitioning(x) == 2
@@ -552,11 +551,9 @@ setMethod("[",
 		plot <- rep(dimnames(tmp)[[1]], each = dim(tmp)[2])
 		abbr.layer <- strsplit(
 			rep(dimnames(tmp)[[2]], dim(tmp)[1]), "@", fixed = TRUE)
-		abbr <- vapply(abbr.layer,
-			FUN = function (x) x[1], FUN.VALUE = character(1))
-		layer <- vapply(abbr.layer,
-			FUN = function (x) x[2], FUN.VALUE = character(1))
-				
+		abbr <- unlist(lapply(abbr.layer, "[[", 1))
+		layer <- unlist(lapply(abbr.layer, "[[", 2))
+		#	class 'species'				
 		res@species.long <- data.frame(plot, abbr, layer, cov,
 			stringsAsFactors = FALSE)
        	res@species.long <- res@species.long[res@species.long$cov != 0, ]
@@ -570,14 +567,12 @@ setMethod("[",
 			stop("NAs introduced in Sites(obj)")
 		}	   
 		if (length(res@group) != 0) {
-		res@group <-
-			res@group[names(res@group) %in%
-				rownames(tmp)]
+		res@group <- res@group[names(res@group) %in% rownames(tmp)]
 		}
 		#	method Abbreviation relies on already subsetted taxonomy!
 		abbr <- sapply(strsplit(names(res), "@", fixed = TRUE),
 		   function (x) x[1])
- 		#	subset taxonomy, layers and spatial slots
+ 		#	finaly subset taxonomy, layers and spatial slots
 		res@taxonomy <- res@taxonomy[res@taxonomy$abbr %in% abbr, ]
 		res@layers <- layer 
 		res@sp.points <- res@sp.points[match(rownames(tmp),
@@ -587,6 +582,15 @@ setMethod("[",
 
 	    return(res)
     }
+)
+
+setReplaceMethod("[", c("VegsoupData", "ANY", "missing", "ANY"), 
+	function(x, i, j, value) {
+		if (!("sites" %in% slotNames(x)))
+			stop("no [[ method for object without slot sites")
+		x@sites[[i]] <- value
+		x
+	}
 )
 
 #	rbind like method to fuse objects

79c1055a90649706df05933220a83771a92498d0
roli <roli@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-02-11 08:41:21 +0000

    removed Matrix from dependencies
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/vegsoup@519 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- install/BuildCheckAndInstall.sh
+++ install/BuildCheckAndInstall.sh
@@ -1,3 +1,3 @@
-R CMD build /Users/roli/Documents/vegsoup/pkg
-R CMD check vegsoup_0.1-3.tar.gz
-R CMD INSTALL -l /Users/roli/Library/R/2.15/library vegsoup_0.1-3.tar.gz
+R CMD build --no-vignettes /Users/roli/Documents/vegsoup/pkg
+R CMD check vegsoup_0.1-7.tar.gz
+R CMD INSTALL -l /Users/roli/Library/R/2.15/library vegsoup_0.1-7.tar.gz

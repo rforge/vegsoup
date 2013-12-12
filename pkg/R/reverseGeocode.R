625a352839db7be6070a7965738153b6a194be51
roli <roli@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-12-12 23:50:22 +0000

    added SpatialPointsDataFrame method fro reverseGeocode
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/vegsoup@573 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/reverseGeocode.R
+++ pkg/R/reverseGeocode.R
@@ -1,17 +1,27 @@
-".reverseGeocode" <- function (lnglat, pm = 100) {
+#	implement route option in method!
+
+".reverseGeocode" <- function (lnglat, pm = 100, route = FALSE) {
 	require(ggmap)
 	require(geonames)	
-	#lnglat <- c(13.297577, 47.612644)
+	#lnglat <- c(12.86217, 48.00019)
 	#pm <- 10	# precision
 	lnglat <- as.numeric(as.character(lnglat))
-	df <- revgeocode(lnglat, output = c("more"))
-	df <- as.data.frame(as.matrix(df), stringsAsFactors = FALSE)
+	r <- revgeocode(lnglat, output = c("more"))
+	r <- as.data.frame(as.matrix(r), stringsAsFactors = FALSE)
 	
-	locality <- paste(
-		df[, grep("country", names(df))], ", ",
-		paste(unique(as.character(df[, rev(grep("administrative", names(df)))])),
-			collapse = ", "), ", ",
-		df[, grep("locality", names(df))],# might be missing
+	r1 <- r[, grep("country", names(r))]
+	r2 <- paste(unique(as.character(r[, rev(grep("administrative", names(r)))])), collapse = ", ")
+	#	eithetr one should be returned?
+	r31 <- unlist(r[, grep("locality", names(r))])
+	r32 <- unlist(r[, grep("route", names(r))])
+	#	r32 mostly not meaningful
+	if (is.null(r31)) {
+		if (!is.null(r32)) r3 <- if (route) r32 else NA else r3 <- NA
+	} else {
+		r3 <- r31
+	}
+	locality <- paste(r1, ", ",	r2,
+		ifelse(!is.na(r3), paste(", ", r3, sep = ""), ""),
 		collapse = ", ", sep = "")
 	options(warn = -1)
 	masl <- GNsrtm3(lat = lnglat[2], lng = lnglat[1])[[1]]
@@ -43,7 +53,7 @@ setGeneric("reverseGeocode",
 
 setMethod("reverseGeocode",
     signature(x = "Vegsoup"),
-    function (x, pm) {
+    function (x) {
     	m <- coordinates(x)[, 1:2]
     	p <- grep("horizontal.precision", names(x))
     	if (length(p) == 1) {
@@ -67,4 +77,42 @@ setMethod("reverseGeocode",
 		x$locality <- sapply(ll, "[[", 2)
     return(x)
     }
-)  
+)
+
+setMethod("reverseGeocode",
+    signature(x = "SpatialPointsDataFrame"),
+    function (x) {
+    	m <- coordinates(x)[, 1:2]
+    	p <- sapply(c("accuracy", "precision"),
+    		function (y) agrep(y, names(x)))
+    	l <- sapply(p, length) > 0
+    	if (any(l)) {
+    		p <- unlist(p[which(l)])
+    	} else {
+    		message("variables accuracy or precision not found")
+    	}
+    	if (length(p) == 1) {
+    		p <- slot(x, "data")[, p]
+    		if (!is.numeric(p)) {
+    			p <- as.numeric(p)
+    			if (any(is.na(p))) {
+    				na <- 20
+    				p[is.na(p)] <- na
+    				message("replace NA with ", na)
+    			}
+    		}
+    	} else {
+    		message("multiple matches for variables accuracy or precision")
+    		p <- rep(NA, nrow(x))
+    	}
+    	ll <- apply(cbind(m, p), 1, function (x) {
+    		.reverseGeocode(x[1:2], x[3])	
+    	}
+    	)
+		coordinate.string <- sapply(ll, "[[", 1)
+		locality <- sapply(ll, "[[", 2)
+		r <- as.matrix(cbind(coordinate.string, locality))
+    return(r)
+    }
+)
+  

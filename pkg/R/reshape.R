970980191127a08c16c5d85c0ebd7fb68178c83e
roli <roli@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-02-16 20:45:32 +0000

    cleaned code
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/vegsoup@587 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/reshape.R
+++ pkg/R/reshape.R
@@ -55,3 +55,27 @@ res <- new("Species", data = res)
 return(invisible(res))
 }
 
+shapeSpecies <- function (obj) {
+	if (!inherits(obj, "Vegsoup")) {
+		stop("only defined for Vegsoup* objects")
+	}
+	spc <- Species(obj)
+	#	data.frame to store results
+	res <- as.data.frame(
+    	     matrix("",
+        	   ncol = length(Layers(obj)) + 2, # we need 2 more columns
+	           nrow = sum(richness(obj, "sample"))),
+    	   stringsAsFactors = FALSE)
+	names(res) <- c("plot", "abbr", Layers(obj))
+
+	res$plot <- rep(rownames(obj), richness(obj, "sample"))
+	res$abbr <- unlist(sapply(rownames(obj), function (x) {
+		Taxonomy(obj[rownames(obj) == x, ])$abbr}))
+	#	slow
+	for (i in 1:nrow(res)) {
+		tmp <- res[i, ]
+	    sel <- tmp$plot == spc$plot & tmp$abbr == spc$abbr
+   		res[i, match(spc[sel, 3], names(res))] <- spc[sel, 4]
+	}
+	return(invisible(res))
+}

67e192940b077d657cb9aee3754744e33ef9aeb0
roli <roli@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-02-24 17:29:07 +0000

    added VegsoupPartition method for vegdist and decostand
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/vegsoup@597 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/decostand.R
+++ pkg/R/decostand.R
@@ -2,23 +2,27 @@
 #	vegan defines:
 #	decostand(x, method, MARGIN, range.global, logbase = 2,
 #	na.rm = FALSE, ...)
+
 #if (!isGeneric("decostand")) {
 setGeneric("decostand",
 	function (x, method, MARGIN, range.global, logbase = 2, na.rm = FALSE, ...)
 	standardGeneric("decostand"))
 #}
+
 #if (!isGeneric("decostand<-")) {
 setGeneric("decostand<-",
 	function (x, value)
 		standardGeneric("decostand<-")
 )
 #}
+
 setMethod("decostand",
 		signature(x = "Vegsoup"),
 	    	function (x) {
 				slot(slot(x, "decostand"), "method")
 			}
 )
+
 setReplaceMethod("decostand",
 	signature(x = "Vegsoup", value = "character"),
 	function (x, value) {
@@ -27,15 +31,47 @@ setReplaceMethod("decostand",
             "standardize", "pa", "chi.square", "hellinger", "log",
             "wisconsin")            
         value <- match.arg(value, METHODS, several.ok = TRUE)		
-		value <- new("decostand", method = value)
-		x@decostand <- value		
+		x@decostand <- new("decostand", method = value)
 		return(x)		
 	}
 )
+
+setReplaceMethod("decostand",
+	signature(x = "VegsoupPartition", value = "character"),
+	function (x, value) {
+		#	taken from vegan
+	    METHODS <- c("total", "max", "frequency", "normalize", "range", 
+            "standardize", "pa", "chi.square", "hellinger", "log",
+            "wisconsin")            
+        value <- match.arg(value, METHODS, several.ok = TRUE)
+ 		
+ 		if (value != decostand(x)) {	
+			x@decostand <- new("decostand", method = value)		
+			#	recompute
+			x <- VegsoupPartition(x, k = getK(x), method = x@method)
+			message("recomputed partitoning")
+		}       		
+		return(x)		
+	}
+)
+
 setReplaceMethod("decostand",
 	signature(x = "Vegsoup", value = "NULL"),
  	function (x, value) {
 		x@decostand <- new("decostand", method = NULL)
+		return(x)
+	}
+)
+
+setReplaceMethod("decostand",
+	signature(x = "VegsoupPartition", value = "NULL"),
+ 	function (x, value) {
+ 		  if (!is.null(decostand(x))) {	
+			x@decostand <- new("decostand", method = NULL)		
+			#	recompute
+			x <- VegsoupPartition(x, k = getK(x))
+			message("recomputed partitoning")
+		}
 		return(x)
 	}
 )
--- pkg/R/vegdist.R
+++ pkg/R/vegdist.R
@@ -24,6 +24,7 @@ setMethod("vegdist",
 		x@dist
 	}
 )
+
 setReplaceMethod("vegdist",
 	signature(x = "Vegsoup", value = "character"),
 	function (x, value) {
@@ -43,15 +44,41 @@ setReplaceMethod("vegdist",
 	}
 )
 
+setReplaceMethod("vegdist",
+	signature(x = "VegsoupPartition", value = "character"),
+	function (x, value) {
+		#	from vegan::vegdist
+		METHODS <- c("manhattan", "euclidean", "canberra", "bray",
+		"kulczynski", "gower", "morisita", "horn", "mountford",
+		"jaccard", "raup", "binomial", "chao", "altGower", "cao")
+		method <- METHODS[pmatch(value, METHODS)]
+    	
+    	if (is.na(method)) 
+        	stop("invalid distance method")
+    	if (method == -1) 
+        	stop("ambiguous distance method")
+        
+        if (method != x@method) {	
+			x@dist <- method		
+			#	recompute
+			x <- VegsoupPartition(x, k = getK(x), method = x@method)
+			message("recomputed partitoning")
+		}
+		return(x)
+	}
+)
+
 #	vegan defines:
 #   distconnected(dis, toolong = 1, trace = TRUE)		
 #	connectedness of dissimilarities
+
 #if (!isGeneric("distconnected<-")) {
 setGeneric("distconnected",
 	function (dis, toolong = 1, trace = TRUE)
 		standardGeneric("distconnected")
 )
 #}
+
 setMethod("distconnected",
 	signature(dis = "Vegsoup"),
 	function (dis, toolong = 1, trace = TRUE) {

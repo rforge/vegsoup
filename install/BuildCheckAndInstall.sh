3496450c2c2d43c1a494a1224c202814df1f3bf3
roli <roli@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-02-24 17:32:02 +0000

    CHECK now with --as-cran
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/vegsoup@599 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- install/BuildCheckAndInstall.sh
+++ install/BuildCheckAndInstall.sh
@@ -1,3 +1,3 @@
 R CMD build /Users/roli/Documents/vegsoup/pkg
-R CMD check vegsoup_0.2-0.tar.gz
+R CMD check --as-cran vegsoup_0.2-0.tar.gz
 R CMD INSTALL -l /Users/roli/Library/R/3.0/library vegsoup_0.2-0.tar.gz

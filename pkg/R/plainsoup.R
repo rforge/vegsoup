bfedf821914d6b044408a57a2ea5a1e94333a6eb
roli <roli@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-03-26 17:15:52 +0000

    added plainsoup function
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/vegsoup@620 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/plainsoup.R
@@ -0,0 +1,23 @@
+plainsoup <- function (m, coverscale, layer = "0l", verbose = TRUE) {
+	if (missing(coverscale))
+		stop("please supply appropiate cover scale of data")
+	stopifnot(inherits(coverscale, "Coverscale"))
+		
+	x <- as.matrix(m)
+	x <- data.frame(
+		abbr = colnames(x),
+		layer = c(rep(layer, ncol(x))),
+		taxon = c(rep(NA, ncol(x))),
+		t(x))
+	x <- stackSpecies(x, verbose = verbose)[, 1:4]
+	
+	z <- taxonomy(data.frame(abbr = unique(abbr(x)), taxon = unique(abbr(x))))
+	
+	y <- data.frame(
+		plot = unique(x$plot),
+		variable = "X1",
+		value = 0)
+	y <- sites(y)	
+
+	r <- Vegsoup(x, y, z, coverscale)
+}

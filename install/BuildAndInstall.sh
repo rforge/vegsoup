904d6c0b3861d48096cef93b8215cafecba81a9a
roli <roli@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-10-22 15:58:38 +0000

    updated install scripts for R 3.1
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/vegsoup@632 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- install/BuildAndInstall.sh
+++ install/BuildAndInstall.sh
@@ -1,5 +1,5 @@
 R CMD build /Users/roli/Documents/vegsoup/pkg
-R CMD INSTALL -d -l /Users/roli/Library/R/3.0/library vegsoup_0.2-1.tar.gz
+R CMD INSTALL -d -l /Users/roli/Library/R/3.1/library vegsoup_0.2-1.tar.gz
 #
 # --fake		do minimal install for testing purposes, this will not install extdata!
 # for wiondows use:

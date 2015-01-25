89aa017bf2fcfc4a0642f2e6e564fb6806e21131
roli <roli@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-01-25 15:07:52 +0000

    cleaned up and delted Association
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/vegsoup@711 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- install/BuildCheckAndInstall.sh
+++ install/BuildCheckAndInstall.sh
@@ -1,3 +1,5 @@
 R CMD build /Users/roli/Documents/vegsoup/pkg
 R CMD check vegsoup_0.2-4.tar.gz
 R CMD INSTALL -l /Users/roli/Library/R/3.1/library vegsoup_0.2-4.tar.gz
+
+#	tools::showNonASCII(readLines())

d4bccfb2b0237ce80f524dd6d64bd62e0b68a233
roli <roli@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-01-28 12:25:13 +0000

    added as.table method
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/vegsoup@727 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/table.R
+++ pkg/R/table.R
@@ -1,1 +1,31 @@
-#	empty moved to files
+setGeneric("as.table")
+
+".as.table.VegsoupPartition" <- function (x, ...) {
+	allargs <- list(...)
+	if (length(allargs) != 1)
+		stop("accept only one column name or vector")
+	
+	n <- allargs[[1]]  # use first element anyway			
+	j <- names(x) == n[1] # column in sites
+	
+	if (!any(j)) {
+		#	'external' vector
+ 		if (length(n) != nrow(x))
+			stop("length of supplied vector must match nrow(x)")
+		else
+			r <- table(as.character(n), partitioning(x))
+	}
+	else {
+		if (any(j))
+			r <- table(as.character(sites(x)[, which(j)]), partitioning(x))
+		else
+			stop("column not found")
+	}	
+	
+	return(r)
+}
+
+setMethod("as.table",
+    signature = "VegsoupPartition",
+	.as.table.VegsoupPartition
+)

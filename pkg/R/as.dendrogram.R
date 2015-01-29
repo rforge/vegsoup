335feba771168bf591ed3040e731ce8805ea6e3c
roli <roli@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-01-29 12:38:15 +0000

    added labels argument to as.dendrogram
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/vegsoup@736 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/as.dendrogram.R
+++ pkg/R/as.dendrogram.R
@@ -24,7 +24,20 @@ setGeneric("as.dendrogram",
 
 setMethod("as.dendrogram",
 	signature(object = "VegsoupPartition"),
-	function (object, table = "constancy", ...) {
+	function (object, table = "constancy", labels = NULL, ...) {
 	r <- as.dendrogram(as.hclust(object, table = table, ...))
+	
+	if (!is.null(labels)) {
+		ll <- unique(cbind(partitioning(object), sites(object)[labels]))
+		leaf <- function (n) {
+			if (is.leaf(n)) {
+				i <- match(as.numeric(attr(n, "label")), ll[,1 ])
+				attr(n, "label") <- as.character(ll[, 2][i])
+			}
+			return(n)
+			}
+	r <- dendrapply(r, leaf)
+	}	
+	return(r)	
 }
 )

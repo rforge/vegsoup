e78a4b373a29c85facf41a261d0b775746a22e8a
roli <roli@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-11-28 17:48:06 +0000

    brought back coenoflex by interfacing coenocline
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/vegsoup@757 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/as.coenocline.Vegsoup.R
@@ -0,0 +1,68 @@
+as.coenocline.Vegsoup <- function (obj) {
+
+	if (attr(obj, "expectation"))
+		stop("expectation must be FALSE")
+
+	#	species
+	X <- obj[]
+	
+	#	sites
+	if (inherits(attr(obj, "locations"), "matrix")) {
+		Y <- as.data.frame(attr(obj, "locations"))
+		names(Y) <- paste0("gradient", 1:ncol(Y))
+	} else {
+		Y <- data.frame(gradient1 = attr(obj, "locations"))	
+	}
+	
+	j <- ncol(X) # number of species
+	i <- nrow(X) # number of plots
+
+	#	cure empty species
+	test <- colSums(X) == 0
+	if (any(test)) {
+		for (k in which(test)) {
+			X[sample(1:nrow(X), size = 1), k] <- 1
+		}
+	}	
+	#	... and empty sites
+	test <- rowSums(X) == 0
+	if (any(test)) {
+		for (k in which(test)) {
+			X[k, sample(1:ncol(X), size = 1)] <- 1
+		}
+	}	
+	
+	#	meaningful names
+	abbr <- sprintf(paste0("spc%0", nchar(j), ".0f"), 1:j)
+	taxon <- sprintf(paste0("Species %0", nchar(j), ".0f"), 1:j)
+	plot <- sprintf(paste0("plt%0", nchar(i), ".0f"), 1:i)
+	
+	#	row-wise index to as.vector()
+	ij <- c(t(matrix(seq_len(i * j), nrow = i, ncol = j)))
+	#	pointer to non-zero values
+	z <- X[ij] != 0
+	
+	X <- matrix(c(
+		rep(plot, each = j)[z],	 # plot
+		rep.int(abbr, i)[z],		 # abbr
+		rep("0l", length(which(z))), # layer
+		round(X[ij[z]], 1)),		 # cov
+		ncol = 4, nrow = table(z)[2])
+	
+	X <- species(X)
+	
+	Y <- stackSites(data.frame(plot = plot, Y))
+	
+	Z <- taxonomy(cbind(abbr, taxon))
+	
+	res <- Vegsoup(X, Y, Z, "percentage")
+	
+	return(res)
+
+}
+
+setAs(from = "coenocline", to = "Vegsoup",
+	def = function (from) {
+		as.coenocline.Vegsoup(from)
+	}
+)
--- pkg/R/coenoflex.R
+++ pkg/R/coenoflex.R
@@ -1,13 +1,43 @@
-#	as long as the package is missing from CRAN
+#	package coenoflex is missing from CRAN since Summer 2014
+#	switched to package coenocliner
 
-coenoflex <- function (numplt = 20, numspc = 10, ...) {	
-#	res <- coenoflex::coenoflex(numgrd = 2, numplt = numplt, numspc = numspc,
-#					 grdtyp = c("e","e"), grdlen = c(1,1), width = c(1/4,1/2),
-#					 variab = c(2,1), grdprd = c(0,0), alphad = c(2,1.8),
-#					 pdist = "r", sdist = "r", skew = 1.5, aacorr = 0.0,
-#					 cmpasy = 0.5, cmpphy = 0, maxtot = 100,
-#					 noise = 5, slack = 0.2, autlin = "irm(1,2)")
-#	
-#	as(res, "Vegsoup")
-message("disfunct")
-}
+coenoflex <- function (n = 30, m = 20, gradients = 2, ...) {
+	#	taken from coenocliner vignette page 9
+	N <- n # number of plots
+	M <- m # number of species
+	
+	# first gradient
+	min1 <- 0
+	max1 <- 1
+	loc1 <- seq(min1, max1, length = N)
+	opt1 <- runif(M, min = min1, max = max1)
+	tol1 <- rep(0.025, M)
+	h <- ceiling(rlnorm(M, meanlog = 3)) # mimic percentages
+	h <- ceiling(h / max(h) * 100)
+	par1 <- cbind(opt = opt1, tol = tol1, h = h)
+	
+	# second gradient
+	min2 <- 0
+	max2 <- 1
+	loc2 <- seq(min2, max2, length = N)
+	opt2 <- runif(M, min = min2, max = max2)
+	tol2 <- ceiling(runif(M, min = 0.05, max = 0.5))
+	par2 <- cbind(opt = opt2, tol = tol2)
+		
+	if (gradients == 1) {
+		locs <- loc1
+		pars <- par1
+	} 
+	
+	if (gradients == 2) {		
+		pars <- list(px = par1, py = par2)
+		locs <- cbind(x = loc1, y = loc2)	
+	} 
+
+	r <- coenocliner::coenocline(locs, responseModel = "gaussian",
+			params = pars,
+			expectation = FALSE)
+	r <- as(r, "Vegsoup")
+			
+	return(r)		
+}	

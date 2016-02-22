27173845ded0603d1b60a839befc65cb048fa112
roli <roli@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-02-22 09:37:15 +0000

    extended compass function
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/vegsoup@824 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/misc.R
+++ pkg/R/misc.R
@@ -1,16 +1,23 @@
 compass  <- function (x) {
-	stopifnot(is.numeric(x))
-	stopifnot(!any(x < 0))
-	r <- cut(x,
-		breaks = c(0, seq(22.5/2, 360, by = 22.5)),
-		labels = c("N","NNE","NE","ENE","E","ESE","SE","SSE",
-			"S","SSW","SW","WSW","W","WNW","NW","NNW"))
-	r[is.na(r)] <- "N"
-	return(as.character(r))
+	b <- c(0, seq(22.5/2, 360, by = 22.5))
+	l <- c("N","NNE","NE","ENE","E","ESE","SE","SSE",
+			"S","SSW","SW","WSW","W","WNW","NW","NNW")
+
+	if (missing(x)) {
+		r <- list(breaks = b, labels = l)
+	} else {
+		stopifnot(is.numeric(x))
+		stopifnot(!any(x < 0))	
+		r <- cut(x,	breaks = b, labels = l)
+		r[is.na(r)] <- "N"
+		r <- as.character(r)
+	}
+	
+	return(r)
 }
 
 singletons <- function (obj) {
-	any(table(partitioning(obj)) == 1)	
+	any(table(partitioning(obj)) == 1)
 }
 
 singleton <- function (obj) {

cb68616f97bf61ffbb7ea74f354e670600a567d3
roli <roli@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-12-15 17:14:18 +0000

    corrected bug in reorder
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/vegsoup@849 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/reorder.R
+++ pkg/R/reorder.R
@@ -1,14 +1,23 @@
 setGeneric("reorder")
 
 ".reorder.VegsoupPartition" <- function (x, dendrogram, ...) {
-	#	get order of leafs in dendrogram
-	d  <- as.dendrogram(dendrogram)
+	#	get order of leafs in dendrogram	
+	if (!inherits(dendrogram, "dendrogram")) {
+		d  <- as.dendrogram(dendrogram)		
+	} else {
+		d <- dendrogram
+	}
+
 	d <- cut(d, 0)$lower
 	d <- sapply(d, function (x) dendrapply(x, function (n) attr(n, "label")))
-	#	partitioning as character
-	p <- factor(partitioning(x))
-	levels(p) <- as.character(order(d))
+	ll <- data.frame(x = as.numeric(names(partitions(x))),
+		d = as.numeric(d), xd = NA, stringsAsFactors = FALSE)
+
+	for (i in ll$d) {
+		ll$xd[ i ] <- which(i == d)
+	}
 	
+	p <- factor(partitioning(x), levels = ll$x, labels = ll$xd)	
 	p <- structure(as.numeric(as.character(p)), names = names(p))
 	x@part <- p	
 	

05e4f88bf0fea158aa97df333c1798636be81199
roli <roli@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-12-15 10:05:44 +0000

    deleted install scripts
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/vegsoup@847 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- install/BuildAndInstall.sh
+++
@@ -1,5 +0,0 @@
-R CMD build --no-build-vignettes /Users/roli/Documents/vegsoup/pkg
-R CMD INSTALL -d -l /Users/roli/Library/R/3.3/library vegsoup_0.2-6.tar.gz
-
-# for coenoflex get it from CRAN archive
-# R CMD INSTALL coenoflex_2.0-1.tar.gz
--- install/BuildCheckAndInstall.sh
+++
@@ -1,6 +0,0 @@
-R CMD build --no-build-vignettes /Users/roli/Documents/vegsoup/pkg
-R CMD check vegsoup_0.2-6.tar.gz
-R CMD INSTALL -l /Users/roli/Library/R/3.3/library vegsoup_0.2-6.tar.gz
-
-#	MacTex needs packages 'hanging', 'inconsolata' and 'helvetic'
-#	tools::showNonASCII(readLines())
